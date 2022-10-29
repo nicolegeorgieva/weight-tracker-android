f2b078c15a4ae21a0f745236643aded21d539e07
Nicole <nicole.georgievva@gmail.com>
2022-10-29 20:52:25 +0300

    Add more info button to BMI screen

--- app/src/main/java/com/weighttracker/BmiScreen.kt
+++ app/src/main/java/com/weighttracker/BmiScreen.kt
@@ -1,11 +1,13 @@
 package com.weighttracker
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.weighttracker.component.BackButton
 import com.weighttracker.component.NumberInputField
@@ -46,9 +48,41 @@ fun BmiScreen() {
 
         Spacer(modifier = Modifier.height(16.dp))
         Text(text = "Your BMI is: ${bmi(height = heightInput, weight = weightInput)}")
+        Spacer(modifier = Modifier.height(16.dp))
+        MoreInfo()
     }
 }
 
 private fun bmi(height: Double, weight: Double): Double {
     return weight / (height * height)
+}
+
+@Composable
+private fun MoreInfo() {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+    Text(
+        text = if (expanded) {
+            "Tap to hide"
+        } else {
+            "Tap for more info"
+        },
+        modifier = Modifier.clickable {
+            expanded = !expanded
+        },
+        color = Color.LightGray,
+        fontWeight = FontWeight.Bold
+    )
+
+    if (expanded) {
+        Text(
+            text = "Body mass index (BMI) is a value derived from the mass (weight) and" +
+                    "height of a person. The BMI is defined as the body mass divided by the square" +
+                    "of the body height, and is expressed in units of kg/m2, resulting from mass in" +
+                    "kilograms and height in metres.",
+            modifier = Modifier,
+            color = Color.DarkGray
+        )
+    }
 }
