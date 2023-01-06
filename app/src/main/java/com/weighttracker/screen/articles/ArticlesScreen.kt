8b9311a30fea7b852e772221becef2848435a264
Nicole <nicole.georgievva@gmail.com>
2023-01-06 15:54:09 +0200

    Articles screen - Improve UI

--- app/src/main/java/com/weighttracker/screen/articles/ArticlesScreen.kt
+++ app/src/main/java/com/weighttracker/screen/articles/ArticlesScreen.kt
@@ -4,12 +4,14 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
@@ -56,13 +58,15 @@ private fun ArticleCard(article: Article) {
             .clickable {
                 browser.openUri(article.articleLink)
             }
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .padding(horizontal = 12.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         AsyncImage(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(200.dp),
+                .height(200.dp)
+                .clip(RoundedCornerShape(8.dp)),
             model = article.image, contentDescription = "",
             contentScale = ContentScale.Crop
         )
