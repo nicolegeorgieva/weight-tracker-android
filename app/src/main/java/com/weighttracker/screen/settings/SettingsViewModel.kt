ec291b2b50e53156eab9b7e216956ffd775145d7
Nicole <nicole.georgievva@gmail.com>
2023-01-10 16:28:45 +0200

    UX improvement

--- app/src/main/java/com/weighttracker/screen/settings/SettingsViewModel.kt
+++ app/src/main/java/com/weighttracker/screen/settings/SettingsViewModel.kt
@@ -5,9 +5,12 @@ import com.weighttracker.persistence.datastore.kgselected.KgSelectedFlow
 import com.weighttracker.persistence.datastore.kgselected.WriteKgSelectedAct
 import com.weighttracker.persistence.datastore.mselected.MSelectedFlow
 import com.weighttracker.persistence.datastore.mselected.WriteMSelectedAct
+import com.weighttracker.persistence.datastore.weight.WeightFlow
+import com.weighttracker.persistence.datastore.weight.WriteWeightAct
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,7 +18,9 @@ class SettingsViewModel @Inject constructor(
     private val kgSelectedFlow: KgSelectedFlow,
     private val writeKgSelectedAct: WriteKgSelectedAct,
     private val mSelectedFlow: MSelectedFlow,
-    private val writeMSelectedAct: WriteMSelectedAct
+    private val writeMSelectedAct: WriteMSelectedAct,
+    private val weightFlow: WeightFlow,
+    private val writeWeightAct: WriteWeightAct
 ) : SimpleFlowViewModel<SettingsState, SettingsEvent>() {
     override val initialUi = SettingsState(
         kg = true, m = true
@@ -34,7 +39,20 @@ class SettingsViewModel @Inject constructor(
     override suspend fun handleEvent(event: SettingsEvent) {
         when (event) {
             is SettingsEvent.KgSelect -> {
+                val kgAlreadySelected = uiState.value.kg //if kgSelected is true
+                val currentWeight = weightFlow(Unit).first()
+
                 writeKgSelectedAct(event.kg) //it saves kg/lb on the phone
+                if (currentWeight != null) {
+                    val convertedWeight = if (kgAlreadySelected && !event.kg) {
+                        currentWeight * 2.205 // to lbs
+                    } else if (!kgAlreadySelected && event.kg) {
+                        currentWeight * 0.45359237
+                    } else {
+                        currentWeight
+                    }
+                    writeWeightAct(convertedWeight)
+                }
             }
             is SettingsEvent.MSelect -> {
                 writeMSelectedAct(event.m)
