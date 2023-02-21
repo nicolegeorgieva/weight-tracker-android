4cc8e2e49959fd47526b9f150f2b9cccb33eb0ce
Nicole <nicole.georgievva@gmail.com>
2023-02-21 14:04:29 +0200

    Refactor glasses function

--- app/src/main/java/com/weighttracker/domain/Water.kt
+++ app/src/main/java/com/weighttracker/domain/Water.kt
@@ -1,54 +1,29 @@
 package com.weighttracker.domain
 
+//We have 4 glasses at the beginning.
+//After last is full new 4 glasses appear.
+//After last is full new 4 glasses appear.
+//Each full glass becomes true.
+
 fun glasses(water: Double): List<Boolean> {
-    return when (water) {
-        in 0.0..0.24 -> listOf(false, false, false, false)
-        in 0.25..0.49 -> listOf(true, false, false, false)
-        in 0.5..0.749 -> listOf(true, true, false, false)
-        in 0.75..0.99 -> listOf(true, true, true, false)
-        in 1.0..1.24 -> listOf(
-            true, true, true, true,
-            false, false, false, false
-        )
-        in 1.25..1.49 -> listOf(
-            true, true, true, true, true,
-            false, false, false
-        )
-        in 1.5..1.749 -> listOf(
-            true, true, true, true,
-            true, true, false, false
-        )
-        in 1.75..1.99 -> listOf(
-            true, true, true, true,
-            true, true, true, false
-        )
-        in 2.0..2.24 -> listOf(
-            true, true, true, true,
-            true, true, true, true,
-            false, false, false, false
-        )
-        in 2.25..2.49 -> listOf(
-            true, true, true, true,
-            true, true, true, true,
-            true, false, false, false
-        )
-        in 2.5..2.749 -> listOf(
-            true, true, true, true,
-            true, true, true, true,
-            true, true, false, false
-        )
-        in 2.75..2.9 -> listOf(
-            true, true, true, true,
-            true, true, true, true,
-            true, true, true, false
-        )
-        in 3.0..3.9 -> listOf(
-            true, true, true, true,
-            true, true, true, true,
-            true, true, true, true
-        )
-        else -> {
-            emptyList()
-        }
+    val list = mutableListOf<Boolean>()
+    val fullGlasses = (water / 0.25).toInt()
+
+    for (i in 1..fullGlasses) {
+        list.add(true)
     }
+
+    val totalGlasses = if (fullGlasses in 0..3) {
+        4
+    } else if (fullGlasses in 4..7) {
+        8
+    } else {
+        12
+    }
+
+    for (i in 1..totalGlasses - fullGlasses) {
+        list.add(false)
+    }
+
+    return list
 }
--- app/src/test/java/com/weighttracker/WaterTest.kt
+++ app/src/test/java/com/weighttracker/WaterTest.kt
@@ -9,18 +9,34 @@ class WaterTest : FreeSpec({
         "0 liters" {
             glasses(0.0) shouldBe listOf(false, false, false, false)
         }
-        "3 liters" {
-            glasses(3.0) shouldBe listOf(
-                true, true, true, true,
-                true, true, true, true,
-                true, true, true, true
-            )
+        "0.25 liters" {
+            glasses(0.25) shouldBe listOf(true, false, false, false)
+        }
+        "0.75 liters" {
+            glasses(0.75) shouldBe listOf(true, true, true, false)
+        }
+        "1 liters" {
+            glasses(1.0) shouldBe listOf(true, true, true, true, false, false, false, false)
         }
         "2 liters" {
             glasses(2.0) shouldBe listOf(
                 true, true, true, true,
                 true, true, true, true,
                 false, false, false, false
+            )
+        }
+        "2.5 liters" {
+            glasses(2.5) shouldBe listOf(
+                true, true, true, true,
+                true, true, true, true,
+                true, true, false, false
+            )
+        }
+        "3 liters" {
+            glasses(3.0) shouldBe listOf(
+                true, true, true, true,
+                true, true, true, true,
+                true, true, true, true
             )
         }
     }
