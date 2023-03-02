b99b96a2d3a31e44a0c51c5f0aadcefa4a87f759
Nicole <nicole.georgievva@gmail.com>
2023-03-02 16:15:12 +0200

    Improve loading and error component's UI

--- app/src/main/java/com/weighttracker/component/ErrorMessage.kt
+++ app/src/main/java/com/weighttracker/component/ErrorMessage.kt
@@ -1,10 +1,13 @@
 package com.weighttracker.component
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -12,17 +15,23 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun ErrorMessage(onClick: () -> Unit) {
-    Text(
-        modifier = Modifier.padding(vertical = 24.dp),
-        text = "Error!",
-        fontSize = 24.sp
-    )
-    Button(
-        onClick = onClick,
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color.DarkGray, contentColor = Color.White
-        )
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Retry")
+        Text(
+            modifier = Modifier.padding(vertical = 24.dp),
+            text = "Error!",
+            color = Color.Red,
+            fontSize = 24.sp
+        )
+        Button(
+            onClick = onClick,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.DarkGray, contentColor = Color.White
+            )
+        ) {
+            Text(text = "Retry")
+        }
     }
 }
--- app/src/main/java/com/weighttracker/component/LoadingMessage.kt
+++ app/src/main/java/com/weighttracker/component/LoadingMessage.kt
@@ -1,19 +1,28 @@
 package com.weighttracker.component
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun LoadingMessage() {
-    Text(
-        modifier = Modifier.padding(24.dp),
-        text = "Loading",
-        fontSize = 24.sp
-    )
-    CircularProgressIndicator()
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            modifier = Modifier.padding(24.dp),
+            text = "Loading",
+            fontSize = 24.sp
+        )
+
+        CircularProgressIndicator()
+    }
 }
