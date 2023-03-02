b272c15b959a9b0bc6dd4893986c71afe038e1ab
Nicole <nicole.georgievva@gmail.com>
2023-03-02 16:11:58 +0200

    Improve loading and error component's UI

--- app/src/main/java/com/weighttracker/component/ErrorMessage.kt
+++ app/src/main/java/com/weighttracker/component/ErrorMessage.kt
@@ -1,13 +1,28 @@
 package com.weighttracker.component
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ErrorMessage(onClick: () -> Unit) {
-    Text(text = "Error!")
-    Button(onClick = onClick) {
+    Text(
+        modifier = Modifier.padding(vertical = 24.dp),
+        text = "Error!",
+        fontSize = 24.sp
+    )
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.DarkGray, contentColor = Color.White
+        )
+    ) {
         Text(text = "Retry")
     }
 }
--- app/src/main/java/com/weighttracker/component/LoadingMessage.kt
+++ app/src/main/java/com/weighttracker/component/LoadingMessage.kt
@@ -1,11 +1,19 @@
 package com.weighttracker.component
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun LoadingMessage() {
-    Text(text = "Loading")
+    Text(
+        modifier = Modifier.padding(24.dp),
+        text = "Loading",
+        fontSize = 24.sp
+    )
     CircularProgressIndicator()
 }
