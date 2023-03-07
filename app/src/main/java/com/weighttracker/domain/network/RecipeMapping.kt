38e1aac4b775932c848ef69ae19fe1f493b6968f
Nicole <nicole.georgievva@gmail.com>
2023-03-07 17:01:21 +0200

    Improve UX (filter recipes with broken link)

--- app/src/main/java/com/weighttracker/domain/network/RecipeMapping.kt
+++ app/src/main/java/com/weighttracker/domain/network/RecipeMapping.kt
@@ -8,12 +8,19 @@ fun mapRecipeResponse(response: RecipeResponse): List<Recipe> {
     return response.feed.mapNotNull { feedItem ->
         val name = feedItem.display?.displayName?.takeIf { it.isNotBlank() }
             ?: return@mapNotNull null
+
+        val link = feedItem.display.source?.sourceRecipeUrl?.takeIf { it.isNotBlank() }
+            ?: return@mapNotNull null
+
+        if (link.contains("https://www.yummly.com/private/")) {
+            return@mapNotNull null
+        }
+
         Recipe(
             image = feedItem.display.images?.firstOrNull().takeIf { it.isNotBlank() }
                 ?: return@mapNotNull null,
             title = name,
-            link = feedItem.display.source?.sourceRecipeUrl?.takeIf { it.isNotBlank() }
-                ?: return@mapNotNull null
+            link = link
         )
     }
 }
