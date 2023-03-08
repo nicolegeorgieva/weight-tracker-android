f370dd66f947a3399e1634a350eaef8fce703b0e
Nicole <nicole.georgievva@gmail.com>
2023-03-08 13:34:11 +0200

    WIP Pagination on Recipe screen

--- app/src/main/java/com/weighttracker/screen/recipe/RecipeScreen.kt
+++ app/src/main/java/com/weighttracker/screen/recipe/RecipeScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -12,15 +13,18 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
-import com.weighttracker.Screens
-import com.weighttracker.browser
+import com.weighttracker.*
+import com.weighttracker.R
 import com.weighttracker.component.ErrorMessage
 import com.weighttracker.component.Header
 import com.weighttracker.component.LoadingMessage
@@ -43,7 +47,38 @@ private fun UI(
     Column(modifier = Modifier.padding(horizontal = 16.dp, vertical = 16.dp)) {
         Header(back = Screens.Settings, title = "Recipes")
 
-        Spacer(modifier = Modifier.height(32.dp))
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                modifier = Modifier
+                    .size(16.dp)
+                    .clickable { navigateTo(Screens.Settings) },
+                painter = painterResource(id = R.drawable.baseline_keyboard_arrow_left_24),
+                contentDescription = "",
+                tint = Color.Black
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Text(text = "1")
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Icon(
+                modifier = Modifier
+                    .size(16.dp)
+                    .clickable { navigateTo(Screens.Settings) },
+                painter = painterResource(id = R.drawable.baseline_keyboard_arrow_right_24),
+                contentDescription = "",
+                tint = Color.Black
+            )
+        }
+
+        Spacer(modifier = Modifier.height(12.dp))
 
         LazyColumn {
             when (state.recipe) {
@@ -100,5 +135,23 @@ private fun RecipeCard(recipe: Recipe) {
             fontWeight = FontWeight.Bold,
             textDecoration = TextDecoration.Underline
         )
+    }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    AppTheme {
+        UI(state = RecipeState(
+            recipe = RemoteCall.Ok(
+                data = listOf(
+                    Recipe(
+                        image = "",
+                        title = "Soup",
+                        link = "",
+                    )
+                )
+            )
+        ), onEvent = {})
     }
 }
